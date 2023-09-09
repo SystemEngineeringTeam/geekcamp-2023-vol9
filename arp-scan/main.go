5fc1ff92d00e8ecd565438bca367ecaa6e5233e9
Bayashin <k21099kk@aitech.ac.jp>
2023-09-09 15:31:45 +0900

    add:接続端末と取得した時間をAPIサーバに送信

---
+++ arp-scan/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/SystemEngineeringTeam/geekcamp-2023-vol9/arp-scan/service"
+)
+
+type netowork struct {
+	RoomId int
+	Ip     string
+	Mask   int
+}
+
+func main() {
+	net := getIpAndMask()
+	// ip := "192.168.0.3"
+	// mask := 24
+	time := service.GetTime()
+	for _, v := range net {
+		c := service.ArpScan(v.Ip, v.Mask)
+		fmt.Println(c)
+		service.PostCount(v.RoomId, c, time)
+	}
+}
+
+func getIpAndMask() []netowork {
+	file, err := os.Open("./network.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	r := csv.NewReader(file)
+	rows, err := r.ReadAll() // csvを一度に全て読み込む
+	if err != nil {
+		log.Fatal(err)
+	}
+	var net []netowork
+	for i, v := range rows {
+		if i == 0 {
+			continue
+		}
+		id, _ := strconv.Atoi(v[0])
+		mask, _ := strconv.Atoi(v[2])
+		n := netowork{
+			RoomId: id,
+			Ip:     v[1],
+			Mask:   mask,
+		}
+		net = append(net, n)
+	}
+	return net
+}
