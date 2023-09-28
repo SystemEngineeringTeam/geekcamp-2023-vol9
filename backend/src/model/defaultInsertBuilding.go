4289f2c92dda3c5ff6cc7869060391a23b7c1a28
harutiro <harutiro2027@outlook.jp>
2023-09-28 13:46:02 +0900

    部屋構成を変更した。

--- backend/src/model/defaultInsertBuilding.go
+++ backend/src/model/defaultInsertBuilding.go
@@ -163,6 +163,277 @@ func BuildingDefaultInsert(){
 						},
 					},
 				},
+				{
+					Floor: 3,
+					Rooms: []Room{
+						{
+							Name: "1号館3F",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+				{
+					Floor: 4,
+					Rooms: []Room{
+						{
+							Name: "1号館4F",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+				{
+					Floor: 5,
+					Rooms: []Room{
+						{
+							Name: "1号館5F-501",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+						{
+							Name: "1号館5F-502",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+				{
+					Floor: 6,
+					Rooms: []Room{
+						{
+							Name: "1号館6F",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+				{
+					Floor: 7,
+					Rooms: []Room{
+						{
+							Name: "1号館7F",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: "10号館",
+			Floors: []Floor{
+				{
+					Floor: 1,
+					Rooms: []Room{
+						{
+							Name: "10号館1F",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+						{
+							Name: "10号館1F-G2110",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+				{
+					Floor: 2,
+					Rooms: []Room{
+						{
+							Name: "10号館2F",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+						{
+							Name: "10号館2F-G2210",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+				{
+					Floor: 3,
+					Rooms: []Room{
+						{
+							Name: "10号館3F",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+				{
+					Floor: 4,
+					Rooms: []Room{
+						{
+							Name: "10号館4F",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+				{
+					Floor: 5,
+					Rooms: []Room{
+						{
+							Name: "10号館5F",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+				{
+					Floor: 6,
+					Rooms: []Room{
+						{
+							Name: "10号館6F",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+				{
+					Floor: 7,
+					Rooms: []Room{
+						{
+							Name: "10号館7F",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: "セントラル",
+			Floors: []Floor{
+				{
+					Floor: 1,
+					Rooms: []Room{
+						{
+							Name: "セントラル",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: "AITプラザ",
+			Floors: []Floor{
+				{
+					Floor: 1,
+					Rooms: []Room{
+						{
+							Name: "AITプラザ",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: "愛和会館",
+			Floors: []Floor{
+				{
+					Floor: 1,
+					Rooms: []Room{
+						{
+							Name: "愛和会館1F",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
+				{
+					Floor: 2,
+					Rooms: []Room{
+						{
+							Name: "愛和会館2F",
+							StayCounts: []StayCount{
+								{
+									StayCount: 0,
+									DateTime: time.Now(),
+								},
+							},
+						},
+					},
+				},
 			},
 		},
 	}
